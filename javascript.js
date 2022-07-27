8ce265c5d3c0e326facfec7a5f8ed83eae404015
rakuzo <rakuzoxilion@gmail.com>
2022-07-27 16:10:32 +0700

    Add Play function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,15 @@
+//Randomly Pick Rock, Paper, or Scissor
 function getComputerChoice () {
     let choice = ["Rock", "Paper", "Scissor"];
     let randChoice = choice[Math.floor(Math.random() * choice.length)];
     return randChoice;
+}
+
+//Play 1 round
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "Rock" && computerSelection === "Scissor") {
+        console.log("You Win!");
+    } else {
+        console.log("You Lose!");
+    }
 }
