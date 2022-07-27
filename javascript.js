76bffd37aac48a1154c8e370a0a3ee8e79fb1310
rakuzo <rakuzoxilion@gmail.com>
2022-07-27 16:22:30 +0700

    Add more condition ro function Play

--- javascript.js
+++ javascript.js
@@ -9,6 +9,16 @@ function getComputerChoice () {
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "Rock" && computerSelection === "Scissor") {
         console.log("You Win! " + playerSelection + " beat " + computerSelection);
+    } else if (playerSelection === "Scissor" && computerSelection === "Paper") {
+        console.log("You Win! " + playerSelection + " beat " + computerSelection);
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        console.log("You Win! " + playerSelection + " beat " + computerSelection)
+    } else if (playerSelection === "Rock" && computerSelection === "Rock") {
+        console.log("You Draw! " + playerSelection + " draw against " + computerSelection)
+    } else if (playerSelection === "Paper" && computerSelection === "Paper") {
+        console.log("You Draw! " + playerSelection + " draw against " + computerSelection)
+    } else if (playerSelection === "Scissor" && computerSelection === "Scissor") {
+        console.log("You Draw! " + playerSelection + " draw against " + computerSelection)
     } else {
         console.log("You Lose! " + computerSelection + " beat " + playerSelection);
     }
