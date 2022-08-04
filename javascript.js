2cd49a8ccb1fd011773cae70196eb1f5bc51d28b
rakuzo <rakuzoxilion@gmail.com>
2022-08-04 14:54:51 +0700

    Add CSS

--- javascript.js
+++ javascript.js
@@ -4,7 +4,6 @@ buttons.forEach(button => button.addEventListener('click', getPlayerChoice));
 
 function getPlayerChoice(e) {
     let playerChoice = this.classList.value;
-    // playRound(playerChoice, getComputerChoice())
     finalGame(playerChoice);
 }
 
@@ -15,7 +14,7 @@ function getComputerChoice () {
     return randChoice;
 }
 
-const result = document.querySelector('#result');
+const round = document.querySelector('#round');
 const para = document.createElement('p');
 
 const score = document.querySelector('#score');
@@ -25,31 +24,31 @@ const paraTwo = document.createElement('p');
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "Rock" && computerSelection === "Scissor") {
         para.textContent = `You Win! ${playerSelection} beat ${computerSelection}`;
-        result.appendChild(para);
+        round.appendChild(para);
         playerScore += 1;
     } else if (playerSelection === "Scissor" && computerSelection === "Paper") {
         para.textContent = `You Win! ${playerSelection} beat ${computerSelection}`;
-        result.appendChild(para);
+        round.appendChild(para);
         playerScore += 1;
     } else if (playerSelection === "Paper" && computerSelection === "Rock") {
         para.textContent = `You Win! ${playerSelection} beat ${computerSelection}`;
-        result.appendChild(para);
+        round.appendChild(para);
         playerScore += 1;
     } else if (playerSelection === "Rock" && computerSelection === "Paper") {
         para.textContent = `You Lose! ${playerSelection} beaten by ${computerSelection}`;
-        result.appendChild(para);
+        round.appendChild(para);
         computerScore += 1;
     } else if (playerSelection === "Paper" && computerSelection === "Scissor") {
         para.textContent = `You Lose! ${playerSelection} beaten by ${computerSelection}`;
-        result.appendChild(para);
+        round.appendChild(para);
         computerScore += 1;
     } else if (playerSelection === "Scissor" && computerSelection === "Rock") {
         para.textContent = `You Lose! ${playerSelection} beaten by ${computerSelection}`;
-        result.appendChild(para);
+        round.appendChild(para);
         computerScore += 1;
     } else {
         para.textContent = `You Tie! Both choose ${computerSelection}`;
-        result.appendChild(para);
+        round.appendChild(para);
         computerScore += 0;
     }
 }
@@ -63,43 +62,13 @@ function finalGame (a) {
     if (playerScore === 5) {
         paraTwo.textContent = `Player Score ${playerScore} VS Computer Score ${computerScore}`;
         score.appendChild(paraTwo);
-        alert('Victory!');
+        // alert('Victory!');
     } else if (computerScore === 5) {
         paraTwo.textContent = `Player Score ${playerScore} VS Computer Score ${computerScore}`;
         score.appendChild(paraTwo);
-        alert('Defeat');
+        // alert('Defeat');
     } else {
         paraTwo.textContent = `Player Score ${playerScore} VS Computer Score ${computerScore}`;
         score.appendChild(paraTwo);
     }
 }
-
-// function game() {
-//     const values = [];    
-//     for (let i = 0; i < 5; i++) {
-//         //Move the variable to this
-//         let computerSelection = getComputerChoice();
-//         let playerSelection = getPlayerChoice();
-
-//         //Can also directly call getPlayerChoice() and also getComputerChoice()
-//         values.push(playRound(playerSelection, computerSelection));
-//     }
-//     return(values);
-// }
-
-// function getFinalResult() {
-//     const finArray = game();
-//     let sum = 0;
-
-//     for (let i = 0; i < finArray.length; i++) {
-//         sum += finArray[i];
-//     }
-
-//     if (sum === 0) {
-//         console.log("You Draw! against Computer");
-//     } else if (sum >= 1) {
-//         console.log("You Win! against Computer");
-//     } else {
-//         console.log("You Lose! against Computer");
-//     }
-// }
